b946df89a16f3e601b44addf6201a0627f170ddb
Marko Rodic <mrmarkorodic@googlemail.com>
2017-05-03 01:52:55 +0100

    Refacted to use props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const DATA = {
+  name: "Roger",
+  imgURL: "https://avatars.githubusercontent.com/perborgen?s=240",
+  hobbyList: ["Eat", "Sleep", "Code"]
+}
+
 class App extends React.Component {
   render() {
     return (
         <div style={{padding: '15px'}}>
-          <Profile />
-          <Hobbies />
+          <Profile myData={DATA} />
+          <Hobbies myData={DATA}/>
         </div>
     )
   }
@@ -17,8 +23,8 @@ class Profile extends React.Component {
   render() {
     return (
       <div>
-        <h1>Mark</h1>
-        <img src="https://avatars.githubusercontent.com/perborgen?s=240" />
+        <h1>{this.props.myData.name}</h1>
+        <img src={this.props.myData.imgURL} />
       </div>
     )
   }
@@ -30,9 +36,11 @@ class Hobbies extends React.Component {
       <div>
         <h3>My hobbies:</h3>
         <ul>
-          <li>Eat</li>
-          <li>Sleep</li>
-          <li>Code</li>
+          {
+            this.props.myData.hobbyList.map((item, index) =>
+              <li key={index}>{item}</li>
+            )
+          }
         </ul>
       </div>    
     )
