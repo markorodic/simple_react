ac01b75e7e686955bc23421218629012dfb46833
Marko Rodic <mrmarkorodic@googlemail.com>
2017-05-03 01:26:51 +0100

    Added profile

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-        <h1>Hello World</h1>
+        <div style={{padding: '15px'}}>
+          <Profile />
+        </div>
+    )
+  }
+}
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Mark</h1>
+        <img src="https://avatars.githubusercontent.com/perborgen?s=240" />
+      </div>
     )
   }
 }
