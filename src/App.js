0cbf770fc2c17514d9eefa11f07d39717fc783d1
Marko Rodic <mrmarkorodic@googlemail.com>
2017-05-03 01:30:36 +0100

    Added hobbies section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
         <div style={{padding: '15px'}}>
           <Profile />
+          <Hobbies />
         </div>
     )
   }
@@ -19,6 +20,21 @@ class Profile extends React.Component {
         <h1>Mark</h1>
         <img src="https://avatars.githubusercontent.com/perborgen?s=240" />
       </div>
+    )
+  }
+}
+
+class Hobbies extends React.Component {
+  render(){
+    return (
+      <div>
+        <h3>My hobbies:</h3>
+        <ul>
+          <li>Eat</li>
+          <li>Sleep</li>
+          <li>Code</li>
+        </ul>
+      </div>    
     )
   }
 }
